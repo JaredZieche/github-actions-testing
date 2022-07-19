f0769a93a98b29bbd23a7f70b379a0eb9d6e271d
jaredzieche <jaredzieche@gmail.com>
2022-07-18 20:55:21 -0400

    updated summary sh

--- job_summary.sh
+++ job_summary.sh
@@ -1,2 +1,3 @@
-echo '## Step script output' >> $GITHUB_STEP_SUMMARY
-echo '# Step script output' >> $GITHUB_STEP_SUMMARY
+echo '## Step script output 1' >> $GITHUB_STEP_SUMMARY
+echo '# Step script output 2' >> $GITHUB_STEP_SUMMARY
+echo 'Step script output 3' >> $GITHUB_STEP_SUMMARY
