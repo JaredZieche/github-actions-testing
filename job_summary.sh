412aec964b3e3edcb2376987d088b1be383f463f
jaredzieche <jaredzieche@gmail.com>
2022-07-18 21:57:11 -0400

    added output grouping

--- job_summary.sh
+++ job_summary.sh
@@ -1,4 +1,6 @@
 echo '## Step script output 1' >> $GITHUB_STEP_SUMMARY
 echo '# Step script output 2' >> $GITHUB_STEP_SUMMARY
 echo 'Step script output 3' >> $GITHUB_STEP_SUMMARY
-echo $RELEASE_VERSION >> $GITHUB_STEP_SUMMARY
+echo "::group::Release Version"
+echo $RELEASE_VERSION is the newest release
+echo "::endgroup::" >> $GITHUB_STEP_SUMMARY
