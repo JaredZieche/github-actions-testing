beae0185505704f4a7325a39e619163ded8da02b
jaredzieche <jaredzieche@gmail.com>
2022-07-18 21:43:03 -0400

    changed input var to envvar

--- job_summary.sh
+++ job_summary.sh
@@ -1,4 +1,4 @@
 echo '## Step script output 1' >> $GITHUB_STEP_SUMMARY
 echo '# Step script output 2' >> $GITHUB_STEP_SUMMARY
 echo 'Step script output 3' >> $GITHUB_STEP_SUMMARY
-echo "${{github.event.inputs.version}}" >> $GITHUB_STEP_SUMMARY
+echo $RELEASE_VERSION >> $GITHUB_STEP_SUMMARY
