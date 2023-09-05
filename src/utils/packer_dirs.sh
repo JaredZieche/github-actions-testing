15f03d68400c1dd70a92a5114197da553b4b9911
jaredzieche <jaredzieche@gmail.com>
2023-09-05 09:56:09 -0400

    adding "debug"

--- src/utils/packer_dirs.sh
+++ src/utils/packer_dirs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo $FILES
 packer_dir=$(echo "${FILES}" | jq -r 'to_entries | map(select(.value | match("src/packer"))) | map(.value) | .[]' | cut -d'/' -f 1-3 | uniq)
 echo $packer_dir
 echo "PACKER_DIR=$(jq -nc --arg packer_dir "${packer_dir}" '{ packer_dirs: $packer_dir | split("\n") }')" >> $GITHUB_OUTPUT
+echo $GITHUB_OUPUT
 echo "# Builds" >> $GITHUB_STEP_SUMMARY
 echo "${packer_dir}" >> $GITHUB_STEP_SUMMARY
